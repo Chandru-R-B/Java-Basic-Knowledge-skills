c14218f7fa7462fa7989ea83977f732b41e498d6
Chandru.R.B <130861022+Chandru-R-B@users.noreply.github.com>
2023-08-07 20:09:39 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class Palindrome{
+void reverse(String org)
+{
+	String rev="";
+	int size=0;
+	size=org.length();
+	for(int i = size-1; i>=0 ; i--)
+		rev = rev+org.charAt(i);
+	if(org.equals(rev))
+		System.out.println("It is a Palindrome");
+	else
+		System.out.println("It is not a palindrome");
+}
+public static void main(String[] args)
+{
+	String org;
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the string :");
+
+	org=sc.nextLine();
+	Palindrome pam = new Palindrome();
+	pam.reverse(org);
+}
+}
